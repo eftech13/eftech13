ea1a4c62a23ae6654efd0ac9c1d9221a0e1b61ca
eftech13 <106220538+eftech13@users.noreply.github.com>
2022-05-31 13:52:17 +0700

    Update installodoopostgrenodocker.sh

--- terraform/gcp/odoo/postgrenodocker/installodoopostgrenodocker.sh
+++ terraform/gcp/odoo/postgrenodocker/installodoopostgrenodocker.sh
@@ -24,6 +24,10 @@ sudo apt install postgresql postgresql-contrib -y
 
 sudo systemctl start postgresql
 sudo systemctl enable postgresql
+sudo bash -c "echo 'host    all             all             0.0.0.0/0               md5' >> /etc/postgresql/12/main/pg_hba.conf"
+sudo bash -c "echo 'listen_addresses = '*'5' >> /etc/postgresql/12/main/postgresql.conf"
+sudo systemctl restart postgresql
+
 
 sudo -u postgres psql -c "CREATE USER tes WITH PASSWORD 'tes'";
 
